64898a617bd2d43e8a71888f2ab73ba4be442225
rumamahato <rumamahato918@gmail.com>
2026-02-12 21:51:24 +0545

    Update settings.py

--- resumeproject/settings.py
+++ resumeproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@a0%m9e3er7m88xcdyzgt9+m0njh$)w9e-1q8$5djx$wc1*pvw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['resume-production-157f.up.railway.app']
 
